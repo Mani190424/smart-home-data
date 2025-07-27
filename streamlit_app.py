04ac39874d16cdb4a5bcfd9beb29dfa9a9ec5b65
mani@1901 <mani1992004@gmail.com>
2025-07-28 01:43:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,10 @@ else:
 
 
 # --- Clean and preprocess ---
-st.write("Loaded Columns:", df.columns.tolist())
+df.columns = df.columns.str.strip()  # clean whitespace
+df.columns = df.columns.str.title()  # converts to Title case
+df['Date'] = pd.to_datetime(df['Date'])
+
 df['Date'] = pd.to_datetime(df['Date'])
 df['Week'] = df['Date'].dt.isocalendar().week
 df['Month'] = df['Date'].dt.to_period('M').astype(str)
